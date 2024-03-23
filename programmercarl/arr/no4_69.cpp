c0b6dce01a8ac24d6b46dbccf4759000241cd570
2515299122 <2515299122@qq.com>
2024-03-23 14:58:18 +0800

    leetcode69.x的平方根

--- programmercarl/arr/no4_69.cpp
+++ programmercarl/arr/no4_69.cpp
@@ -2,25 +2,20 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        //计算x二进制位数
-        int count = 0, tmp = x;
-        while (tmp) {
-            tmp >>= 1;
-            count++;
-        }
-        int left = 0, right = 1<<count, mid, result;
+        long left = 0, mid, ans, right = x, result;
         while (left <= right) {
             mid = left + ((right - left) >> 2);
             result = mid * mid;
-            if (result > x){
-                right=mid-1;
-            } else if(result<x){
-                left=mid+1;
-            } else{
+            if (result > x) {
+                right = mid - 1;
+            } else if (result < x) {
+                ans = mid;//保留小于的结果，实在找不到就向下取整
+                left = mid + 1;
+            } else {
                 return mid;
             }
         }
-        return mid;
+        return ans;
     }
 };
 int main(){
