b5fff2a68aa49972f369a7eb98197096be9956b7
2515299122 <2515299122@qq.com>
2024-03-25 21:05:28 +0800

    leetcode977.有序数组的平方

---
+++ programmercarl/arr/no10_977.cpp
@@ -0,0 +1,67 @@
+//给你一个按 非递减顺序 排序的整数数组 nums，返回 每个数字的平方 组成的新数组，要求也按 非递减顺序 排序。
+//
+//
+//
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：nums = [-4,-1,0,3,10]
+//输出：[0,1,9,16,100]
+//解释：平方后，数组变为 [16,1,0,9,100]
+//排序后，数组变为 [0,1,9,16,100]
+//
+// 示例 2：
+//
+//
+//输入：nums = [-7,-3,2,3,11]
+//输出：[4,9,9,49,121]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= nums.length <= 10⁴
+// -10⁴ <= nums[i] <= 10⁴
+// nums 已按 非递减顺序 排序
+//
+//
+//
+//
+// 进阶：
+//
+//
+// 请你设计时间复杂度为 O(n) 的算法解决本问题
+//
+//
+// Related Topics 数组 双指针 排序 👍 973 👎 0
+#include <vector>
+using namespace std;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        int res1=0,res2=0;
+        int left=0,right=nums.size()-1,cur=nums.size();
+        vector<int> v;
+        v.resize(nums.size());
+        while (left<=right){
+            res1=nums[left]*nums[left];
+            res2=nums[right]*nums[right];
+            if(res1>res2){
+                v[--cur]=res1;
+                left++;
+            } else{
+                v[--cur]=res2;
+                right--;
+            }
+        }
+        return v;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
