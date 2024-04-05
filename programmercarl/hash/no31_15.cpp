fb98b77a885714a6b5859d552261decfc87fed32
2515299122 <2515299122@qq.com>
2024-04-05 15:42:59 +0800

    leetcode15.三数之和

---
+++ programmercarl/hash/no31_15.cpp
@@ -0,0 +1,86 @@
+//给你一个整数数组 nums ，判断是否存在三元组 [nums[i], nums[j], nums[k]] 满足 i != j、i != k 且 j !=
+//k ，同时还满足 nums[i] + nums[j] + nums[k] == 0 。请
+//
+// 你返回所有和为 0 且不重复的三元组。
+//
+// 注意：答案中不可以包含重复的三元组。
+//
+//
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：nums = [-1,0,1,2,-1,-4]
+//输出：[[-1,-1,2],[-1,0,1]]
+//解释：
+//nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0 。
+//nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0 。
+//nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0 。
+//不同的三元组是 [-1,0,1] 和 [-1,-1,2] 。
+//注意，输出的顺序和三元组的顺序并不重要。
+//
+//
+// 示例 2：
+//
+//
+//输入：nums = [0,1,1]
+//输出：[]
+//解释：唯一可能的三元组和不为 0 。
+//
+//
+// 示例 3：
+//
+//
+//输入：nums = [0,0,0]
+//输出：[[0,0,0]]
+//解释：唯一可能的三元组和为 0 。
+//
+//
+//
+//
+// 提示：
+//
+//
+// 3 <= nums.length <= 3000
+// -10⁵ <= nums[i] <= 10⁵
+//
+//
+// Related Topics 数组 双指针 排序 👍 6798 👎 0
+
+#include <vector>
+#include <algorithm>
+using namespace std;
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> vec;
+        sort(nums.begin(),nums.end());
+        for (int i = 0; i < nums.size(); ++i) {
+            if(nums[i]>0)break;
+            //a去重
+            if(i>0&&nums[i]==nums[i-1])continue;
+            int left=i+1,right=nums.size()-1;
+            while (left<right){
+                int sum=nums[i]+nums[left]+nums[right];
+                if(sum>0){
+                    right--;
+                }else if(sum<0){
+                    left++;
+                } else{
+                    //b去重
+                    while (left+1<right&&nums[left]==nums[left+1]) left++;
+                    //c去重
+                    while (right-1>left&&nums[right]==nums[right-1]) right--;
+                    vec.push_back({nums[i],nums[left],nums[right]});
+                    left++;
+                    right--;
+                }
+            }
+        }
+        return vec;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
