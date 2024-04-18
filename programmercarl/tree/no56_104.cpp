1a9b7280ab4de505eff3460383137d899a0d8df1
2515299122 <2515299122@qq.com>
2024-04-18 14:08:43 +0800

    leetcode104.二叉树的最大深度

---
+++ programmercarl/tree/no56_104.cpp
@@ -0,0 +1,81 @@
+//给定一个二叉树 root ，返回其最大深度。
+//
+// 二叉树的 最大深度 是指从根节点到最远叶子节点的最长路径上的节点数。
+//
+//
+//
+// 示例 1：
+//
+//
+//
+//
+//
+//
+//输入：root = [3,9,20,null,null,15,7]
+//输出：3
+//
+//
+// 示例 2：
+//
+//
+//输入：root = [1,null,2]
+//输出：2
+//
+//
+//
+//
+// 提示：
+//
+//
+// 树中节点的数量在 [0, 10⁴] 区间内。
+// -100 <= Node.val <= 100
+//
+//
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树 👍 1817 👎 0
+#include <queue>
+
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode *root) {
+        if (root == nullptr) return 0;
+        queue < TreeNode * > q;
+        int height=0;
+        q.push(root);
+        while (!q.empty()) {
+            height++;
+            int size = q.size();
+            for (int i = 0; i < size; ++i) {
+                auto node = q.front();
+                q.pop();
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+        }
+        return height;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
