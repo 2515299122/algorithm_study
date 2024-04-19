4a9f769df246c27db51a95aeabbe893f74496981
2515299122 <2515299122@qq.com>
2024-04-19 09:29:23 +0800

    leetcode101.对称二叉树

---
+++ programmercarl/tree/no59_101.cpp
@@ -0,0 +1,75 @@
+//给你一个二叉树的根节点 root ， 检查它是否轴对称。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：root = [1,2,2,3,4,4,3]
+//输出：true
+//
+//
+// 示例 2：
+//
+//
+//输入：root = [1,2,2,null,3,null,3]
+//输出：false
+//
+//
+//
+//
+// 提示：
+//
+//
+// 树中节点数目在范围 [1, 1000] 内
+// -100 <= Node.val <= 100
+//
+//
+//
+//
+// 进阶：你可以运用递归和迭代两种方法解决这个问题吗？
+//
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树 👍 2699 👎 0
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool compare(TreeNode *lChild, TreeNode *rChild) {
+        if (lChild && !rChild) return false;
+        if (!lChild && rChild) return false;
+        if (!lChild && !rChild) return true;
+        return lChild->val == rChild->val
+               && compare(lChild->left, rChild->right)
+               && compare(lChild->right, rChild->left);
+    }
+
+    bool isSymmetric(TreeNode *root) {
+        if (root == nullptr) return true;
+        return compare(root->left, root->right);
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
