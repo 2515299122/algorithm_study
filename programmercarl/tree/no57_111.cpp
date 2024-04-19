10a19805c211f2d816fb0f08a83c2b157a7cb1c9
2515299122 <2515299122@qq.com>
2024-04-19 08:57:43 +0800

    leetcode111.二叉树的最小深度

---
+++ programmercarl/tree/no57_111.cpp
@@ -0,0 +1,83 @@
+//给定一个二叉树，找出其最小深度。
+//
+// 最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+//
+// 说明：叶子节点是指没有子节点的节点。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：root = [3,9,20,null,null,15,7]
+//输出：2
+//
+//
+// 示例 2：
+//
+//
+//输入：root = [2,null,3,null,4,null,5,null,6]
+//输出：5
+//
+//
+//
+//
+// 提示：
+//
+//
+// 树中节点数的范围在 [0, 10⁵] 内
+// -1000 <= Node.val <= 1000
+//
+//
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树 👍 1183 👎 0
+#include <queue>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode *root) {
+        if (root == nullptr) return 0;
+        queue<TreeNode *> queue;
+        queue.push(root);
+        int min_depth=1;
+        while (!queue.empty()){
+            int size=queue.size();
+            for (int i = 0; i < size; ++i) {
+                auto node=queue.front();
+                queue.pop();
+                if(node->left== nullptr&&node->right== nullptr){
+                    return min_depth;
+                }
+                if(node->left) queue.push(node->left);
+                if(node->right) queue.push(node->right);
+            }
+            min_depth++;
+        }
+        return min_depth;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
