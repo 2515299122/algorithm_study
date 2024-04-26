342bc3069a4258beea265d3eaf25e1f8a2d5f9af
2515299122 <2515299122@qq.com>
2024-04-26 09:09:35 +0800

    leetcode105.从前序与中序遍历序列构造二叉树

---
+++ programmercarl/tree/no68_105.cpp
@@ -0,0 +1,77 @@
+//给定两个整数数组 preorder 和 inorder ，其中 preorder 是二叉树的先序遍历， inorder 是同一棵树的中序遍历，请构造二叉树并
+//返回其根节点。
+//
+//
+//
+// 示例 1:
+//
+//
+//输入: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+//输出: [3,9,20,null,null,15,7]
+//
+//
+// 示例 2:
+//
+//
+//输入: preorder = [-1], inorder = [-1]
+//输出: [-1]
+//
+//
+//
+//
+// 提示:
+//
+//
+// 1 <= preorder.length <= 3000
+// inorder.length == preorder.length
+// -3000 <= preorder[i], inorder[i] <= 3000
+// preorder 和 inorder 均 无重复 元素
+// inorder 均出现在 preorder
+// preorder 保证 为二叉树的前序遍历序列
+// inorder 保证 为二叉树的中序遍历序列
+//
+//
+// Related Topics 树 数组 哈希表 分治 二叉树 👍 2285 👎 0
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder) {
+        if (preorder.empty()) return nullptr;
+        int pos = ranges::find(inorder, preorder.front()) - inorder.begin();
+        vector<int> pre1(preorder.begin() + 1, preorder.begin() + 1 + pos);
+        vector<int> pre2(preorder.begin() + 1 + pos, preorder.end());
+        vector<int> in1(inorder.begin(), inorder.begin() + pos);
+        vector<int> in2(inorder.begin() + pos + 1, inorder.end());
+        auto left = buildTree(pre1, in1);
+        auto right = buildTree(pre2, in2);
+        return new TreeNode(preorder.front(), left, right);
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
